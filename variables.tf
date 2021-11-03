96c0c9060771ea928c0bf128499bb918b9248121
lrmiguel <leo.rmiguel@gmail.com>
2021-11-03 08:37:55 -0300

    feat: :sparkles: create cluster and nodes

--- variables.tf
+++ variables.tf
@@ -1,3 +1,22 @@
 variable "prefix" {
   type = string
 }
+variable "cluster_name" {
+
+}
+
+variable "retention_days" {
+
+}
+
+variable "desired_size" {
+
+}
+
+variable "max_size" {
+
+}
+
+variable "min_size" {
+
+}
